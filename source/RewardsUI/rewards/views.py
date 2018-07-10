c24f622b455218152a75ef34edacbb69f6507ac2
Geoff Lentsch <glentsch@gnostech.com>
2018-07-10 01:24:30 -0400

    Handles all of the server calls and interactions with the rewards views

--- source/RewardsUI/rewards/views.py
+++ source/RewardsUI/rewards/views.py
@@ -3,22 +3,38 @@ import requests
 
 from django.template.response import TemplateResponse
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
 
+from conf import SERVER_URL
+from .forms import Rewards_Form
 
 class RewardsView(TemplateView):
-    template_name = 'index.html'
-
+    template_name = 'rewards.html'
+    rewards_url = '{}rewards'.format(SERVER_URL)
     def __init__(self, logger=logging.getLogger(__name__)):
         self.logger = logger
 
     def get(self, request, *args, **kwargs):
         context = self.get_context_data(**kwargs)
-
-        response = requests.get("http://rewardsservice:7050/rewards")
+        context['form'] = Rewards_Form()
+        response = requests.get(RewardsView.rewards_url)
         context['rewards_data'] = response.json()
-
+        if not 'customers' in context:
+            response = requests.get('{}customers/'.format(SERVER_URL))
+            context['customers'] = response.json()
         return TemplateResponse(
             request,
             self.template_name,
             context
         )
+    
+    def post(self, request, *args, **kwargs):
+        form = Rewards_Form(request.POST)
+        user_reward = None
+        if form.is_valid():
+            f = form.process()
+            print(f['email'])
+            response = requests.get('{}customers/{}/'.format(SERVER_URL, f['email']))
+            user_reward = response.json() #TODO: should handle other than 200
+            user_reward['progress'] *= 100
+        return self.get(request, customers=[user_reward])
